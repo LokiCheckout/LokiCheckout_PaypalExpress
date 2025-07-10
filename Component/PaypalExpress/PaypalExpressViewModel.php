6a19c4c28501ba6c792a6f7d7022e73c75c8b65d
Jisse Reitsma <jisse@yireo.com>
2025-07-10 14:39:51 +0200

    Adding all files to git via Yireo Command

--- Component/PaypalExpress/PaypalExpressViewModel.php
+++ Component/PaypalExpress/PaypalExpressViewModel.php
@@ -1,6 +1,6 @@
 <?php declare(strict_types=1);
 
-namespace LokiCheckout\PaypalExpress\Component\PaypalExpress;
+namespace Loki\Checkout\PaypalExpress\Component\PaypalExpress;
 
 use Magento\Framework\Data\Form\FormKey;
 use Yireo\LokiCheckout\ViewModel\CheckoutState;
